f79b5494208095e6eabb1e93f50037e2824aa87f
Danilo Souza <daakemii@gmail.com>
2025-04-29 17:23:58 -0300

    Ajuste no Runners

--- src/main/java/org/suzano/rest/runners/Runners.java
+++ src/main/java/org/suzano/rest/runners/Runners.java
@@ -1,7 +1,18 @@
 package org.suzano.rest.runners;
 
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-reports/index.html", "json:target/cucumber-reports/cucumber.json"},
+        features = "src/test/resources/features",
+        glue = "org.suzano.rest.steps",
+        monochrome = true,
+        tags = "@regressive"
+)
+
+@RunWith(Cucumber.class)
 public class Runners {
-
 }
